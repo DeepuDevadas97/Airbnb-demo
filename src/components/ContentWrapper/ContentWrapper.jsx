00c8537d62d07123f6e6da2340f17fec0f7b3dc6
DeepuDevadas97 <deepu.devadas1997@gmail.com>
2025-06-08 17:58:57 +0530

    animation addes

--- src/components/ContentWrapper/ContentWrapper.jsx
+++ src/components/ContentWrapper/ContentWrapper.jsx
@@ -1,31 +1,3 @@
-
-// import RightArrow from "../../assets/icons/RightArrow";
-// import MainSlider from "../MainSlider/MainSlider";
-
-// const ContentWrapper = ({title}) => {
-//   return (
-//     <div className="container">
-//       <div className="mb-4 max-w-[400px]">
-//         <a href="#" className="inline-flex items-center gap-1">
-//           <h2 className="text-[20px] md:text-[22px] leading-[24px] font-bold text-black">
-//             {title}
-//           </h2>{" "}
-//           <span className="mt-1">
-//             <RightArrow />
-//           </span>
-//         </a>
-//       </div>
-//       <div>
-//         <MainSlider/>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ContentWrapper;
-
-
-
 import RightArrow from "../../assets/icons/RightArrow";
 import MainSlider from "../MainSlider/MainSlider";
 
